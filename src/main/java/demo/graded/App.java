dd16bab48f17f3ac47139ba990889fda667dbefe
Glency Swetha J <glencyswetha.j@hcl.com>
2021-12-11 23:55:47 +0530

    Initial commit

---
+++ src/main/java/demo/graded/App.java
@@ -0,0 +1,13 @@
+package demo.graded;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Devops!" );
+    }
+}
